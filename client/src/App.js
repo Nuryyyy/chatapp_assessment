6f95dafd62ea100a0aa96f33e9c4cd6b863544c2
Nuryyyy <a.nurymar@yahoo.com>
2023-02-23 22:12:59 +0800

    added the chat page and the design to it as well

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,22 @@ import './App.css';
 import Chat from './pages/Chat';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import SetProfile from './pages/SetProfile';
+import SetProfile from './components/SetProfile';
+import SetProfileV2 from './components/SetProfileV2'
+import RegisterV2 from './pages/RegisterV2'
+import ChatV2 from './pages/ChatV2';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/register' element={<Register/>} />
+        {/* <Route path='/registerV2' element={<RegisterV2/>} /> */}
         <Route path='/login' element={<Login/>} />
         <Route path='/setProfile' element={<SetProfile/>} />
+        <Route path='/setProfileV2' element={<SetProfileV2/>} />
         <Route path='/' element={<Chat/>} />
+        <Route path='/chatv2' element={<ChatV2/>} />
       </Routes>
     
     </BrowserRouter>
